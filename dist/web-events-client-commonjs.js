5e947dbe3676a332eccfbed012a0b3d340c18358
Dmitry Lozhkin <31573763+Coder911th@users.noreply.github.com>
2018-03-30 08:38:06 +0300

    increment version of commonjs

--- dist/web-events-client-commonjs.js
+++ dist/web-events-client-commonjs.js
@@ -10,7 +10,7 @@ function webEvents(serverURL, evs) {
     }
 
     // Версия web-events-client
-    var VERSION = '2.1.0';
+    var VERSION = '2.2.0';
 
     /*
         Обёртка над пользовательским событием
